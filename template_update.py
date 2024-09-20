cf90992dd628ecdd78f5364cecc777d7a78ea67c
Sudipta Kar <sudipkar@amazon.com>
2024-09-20 16:43:31 -0700

    older image aug, oct, dec 23

--- template_update.py
+++ template_update.py
@@ -2,32 +2,35 @@ import argparse
 import shutil
 import os
 
+iter_path = "iterations"
+template_folder = "pMQseJPcaH"
+
+
 def get_argparser():
     parser = argparse.ArgumentParser(description='OpenReview venue management')
     parser.add_argument('--month', required=True, help='which month?')
     parser.add_argument('--full_copy', action='store_true', help='when creating new, otherwise only need to copy template')
     return parser
 
-args = get_argparser().parse_args()
-iter_path = "iterations"
-template_folder = "pMQseJPcaH"
+if __name__ == '__main__':
+    args = get_argparser().parse_args() 
 
-os.chdir(os.path.join(os.getcwd(), iter_path))
+    os.chdir(os.path.join(os.getcwd(), iter_path))
 
-if args.month == "all":
-    years = os.listdir()
-    years.remove(template_folder)
-    months = [os.path.join(y,m) for y in years for m in os.listdir(y)]
-    # months = [a for a in os.listdir() if os.path.isdir(a)]
-else:
-    months = [args.month]
-
-print(months)
-
-for month in months:
-    if not os.path.exists(month) or args.full_copy:
-        print("Performing full copy")
-        shutil.copytree(template_folder, month)
+    if args.month == "all":
+        years = os.listdir()
+        years.remove(template_folder)
+        months = [os.path.join(y,m) for y in years for m in os.listdir(y)]
+        # months = [a for a in os.listdir() if os.path.isdir(a)]
     else:
-        print("Performing template copy")
-        shutil.copy(os.path.join(template_folder, "index.html"), month)
+        months = [args.month]
+
+    print(months)
+
+    for month in months:
+        if not os.path.exists(month) or args.full_copy:
+            print("Performing full copy")
+            shutil.copytree(template_folder, month)
+        else:
+            print("Performing template copy")
+            shutil.copy(os.path.join(template_folder, "index.html"), month)
